b5f45562cace51e0015c40044a5f8c37dc5c87c6
anthonychan1211 <anthonychan1211@gmail.com>
2025-04-02 21:59:03 +0100

    Step 1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
+import handler from "./api/api";
+
 export default function Home() {
+  useEffect(() => {
+    async function getPokemon() {
+      const response = await fetch("/api/api");
+      const data = await response.json();
+      console.log(data);
+    }
+    getPokemon();
+  });
   return (
     <div>
       {/* Your code goes here. Have fun! */}
+      <h1>Hi</h1>
     </div>
-  )
+  );
 }
