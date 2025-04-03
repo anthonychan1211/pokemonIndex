f7e51038b8db07bbff289145c399c6aff9318083
anthonychan1211 <anthonychan1211@gmail.com>
2025-04-03 22:52:51 +0100

    Step 1

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,7 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
-  const data = await response.json();
-  res.status(200).json(data);
+  let pokemonList = [];
+  for (let i = 1; i <= 151; i++) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    const { id, stats, types, name } = await response.json();
+    pokemonList.push({ id, stats, name, types });
+  }
+
+  console.log(pokemonList);
+  res.status(200).json(pokemonList);
 }
